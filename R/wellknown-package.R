d6fbe36b57575009d8b4b52aac82c39f9fca1fec
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-22 08:55:28 -0800

    added pkg level man file

---
+++ R/wellknown-package.R
@@ -0,0 +1,9 @@
+#' WKT to GeoJSON and vice versa
+#'
+#' @name wellknown-package
+#' @aliases wellknown
+#' @docType package
+#' @title WKT to GeoJSON and vice versa
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords package
+NULL
