fbed7885a3eb9f09d11373f12753d39cc2953009
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-21 17:59:00 -0800

    changed to using single fxn to interface

--- R/wkt_r.R
+++ R/wkt_r.R
@@ -1,22 +1,42 @@
-# point <- list('type' = 'Point', 'coordinates' = c(116.4, 45.2, 11.1))
-# point
-#
-# decimals <- 16
-# coords <- point$coordinates
-# sprintf('POINT (%s)', paste0(format(coords, nsmall = decimals), collapse = ""))
-
-#' Convert GeoJSON-like POINT object to WKT.
+#' Convert GeoJSON-like objects to WKT.
 #'
 #' @export
 #'
-#' @param obj A GeoJSON-like `dict` representing a Point.
+#' @param obj A GeoJSON-like object representing a Point, LineString, Polygon, MultiPolygon, etc.
 #' @param fmt Format string which indicates the number of digits to display after the
 #' decimal point when formatting coordinates.
-#' @return WKT representation of the input GeoJSON Point ``obj``.
 #' @examples
+#' # point
 #' point <- list('type' = 'Point', 'coordinates' = c(116.4, 45.2, 11.1))
-#' dump_point(point)
+#' gj2wkt(point)
+#'
+#' # linestring
+#' st <- list(type = 'LineString',
+#'            coordinates = list(c(0.0, 0.0, 10.0), c(2.0, 1.0, 20.0),
+#'                              c(4.0, 2.0, 30.0), c(5.0, 4.0, 40.0)))
+#' gj2wkt(st)
+#'
+#' # polygon
+#' poly <- list(type = 'Polygon',
+#'      coordinates=list(
+#'        list(c(100.001, 0.001), c(101.12345, 0.001), c(101.001, 1.001), c(100.001, 0.001)),
+#'        list(c(100.201, 0.201), c(100.801, 0.201), c(100.801, 0.801), c(100.201, 0.201))
+#' ))
+#' gj2wkt(poly)
+#' gj2wkt(poly, fmt=6)
 
+gj2wkt <- function(obj, fmt = 16){
+  switch(tolower(obj$type),
+         point = dump_point(obj, fmt),
+         linestring = dump_linestring(obj, fmt),
+         polygon = dump_polygon(obj, fmt)
+  )
+}
+
+#' Convert GeoJSON-like POINT object to WKT.
+#'
+#' @inheritParams gj2wkt
+#' @keywords internal
 dump_point <- function(obj, fmt = 16){
   coords <- obj$coordinates
   sprintf('POINT (%s)', paste0(format(coords, nsmall = fmt), collapse = ""))
@@ -24,16 +44,8 @@ dump_point <- function(obj, fmt = 16){
 
 #' Convert GeoJSON-like LineString object to WKT.
 #'
-#' @export
-#'
-#' @param obj A GeoJSON-like `dict` representing a LineString
-#' @param fmt Format string which indicates the number of digits to display after the
-#' decimal point when formatting coordinates.
-#' @examples
-#' st <- list(type = 'LineString',
-#'            coordinates = list(c(0.0, 0.0, 10.0), c(2.0, 1.0, 20.0),
-#'                              c(4.0, 2.0, 30.0), c(5.0, 4.0, 40.0)))
-#' dump_linestring(st, fmt=0)
+#' @inheritParams gj2wkt
+#' @keywords internal
 dump_linestring <- function(obj, fmt = 16){
   coords <- obj$coordinates
   str <- paste0(lapply(coords, function(z){
@@ -44,19 +56,8 @@ dump_linestring <- function(obj, fmt = 16){
 
 #' Convert GeoJSON-like Polygon object to WKT.
 #'
-#' @export
-#'
-#' @param obj A GeoJSON-like `dict` representing a LineString
-#' @param fmt Format string which indicates the number of digits to display after the
-#' decimal point when formatting coordinates.
-#' @examples
-#' poly <- list(type = 'Polygon',
-#'      coordinates=list(
-#'        list(c(100.001, 0.001), c(101.12345, 0.001), c(101.001, 1.001), c(100.001, 0.001)),
-#'        list(c(100.201, 0.201), c(100.801, 0.201), c(100.801, 0.801), c(100.201, 0.201))
-#' ))
-#' dump_polygon(poly)
-#' dump_polygon(poly, fmt=6)
+#' @inheritParams gj2wkt
+#' @keywords internal
 dump_polygon <- function(obj, fmt = 16){
   coords <- obj$coordinates
   str <- paste0(lapply(coords, function(z){
