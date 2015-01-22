8c2bf92f93f39d5c54d74719ad6e4db10f867286
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-21 17:06:20 -0800

    updated readme, added linestring fxn

--- R/wkt_r.R
+++ R/wkt_r.R
@@ -17,8 +17,27 @@
 #' point <- list('type' = 'Point', 'coordinates' = c(116.4, 45.2, 11.1))
 #' dump_point(point)
 
-dump_point <- function(obj, fmt){
-  decimals <- 16
-  coords <- point$coordinates
-  sprintf('POINT (%s)', paste0(format(coords, nsmall = decimals), collapse = ""))
+dump_point <- function(obj, fmt = 16){
+  coords <- obj$coordinates
+  sprintf('POINT (%s)', paste0(format(coords, nsmall = fmt), collapse = ""))
+}
+
+#' Convert GeoJSON-like LineString object to WKT.
+#'
+#' @export
+#'
+#' @param obj A GeoJSON-like `dict` representing a LineString
+#' @param fmt Format string which indicates the number of digits to display after the
+#' decimal point when formatting coordinates.
+#' @examples
+#' st <- list(type = 'LineString',
+#'            coordinates = list(c(0.0, 0.0, 10.0), c(2.0, 1.0, 20.0),
+#'                              c(4.0, 2.0, 30.0), c(5.0, 4.0, 40.0)))
+#' dump_linestring(st, fmt=0)
+dump_linestring <- function(obj, fmt = 16){
+  coords <- obj$coordinates
+  str <- paste0(lapply(coords, function(z){
+    paste0(gsub("\\s", "", format(z, nsmall = fmt)), collapse = " ")
+  }), collapse = ", ")
+  sprintf('LINESTRING (%s)', str)
 }
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 wellknown
 =======
 
+
+
 `wellknown` - convert WKT and WKB to GeoJSON.
 
 ## Install
@@ -24,8 +26,17 @@ library("wellknown")
 ```r
 point <- list('type' = 'Point', 'coordinates' = c(116.4, 45.2, 11.1))
 dump_point(point)
+#> [1] "POINT (116.4000000000000057 45.2000000000000028 11.0999999999999996)"
 ```
 
+### LineString
+
+
+```r
+st <- list(type = 'LineString',
+            coordinates = list(c(0.0, 0.0, 10.0), c(2.0, 1.0, 20.0),
+                              c(4.0, 2.0, 30.0), c(5.0, 4.0, 40.0)))
+dump_linestring(st, fmt=0)
+#> [1] "LINESTRING (0 0 10, 2 1 20, 4 2 30, 5 4 40)"
 ```
-## [1] "POINT (116.4000000000000057 45.2000000000000028 11.0999999999999996)"
-```
+
