93551ada6d99119291df8cf176ffc69b0be6632b
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-14 08:32:29 -0700

    modified as.json man file

--- R/as.json.R
+++ R/as.json.R
@@ -16,10 +16,11 @@
 #'    (103.2 0.2, 104.8 0.2, 100.8 0.8, 103.2 0.2))"
 #' as.json(wkt2geojson(str))
 #' as.json(wkt2geojson(str), FALSE)
-as.json <- function(x, pretty=TRUE, auto_unbox=TRUE, ...) UseMethod("as.json")
+as.json <- function(x, pretty=TRUE, auto_unbox=TRUE, ...) {
+  UseMethod("as.json")
+}
 
 #' @export
-#' @rdname as.json
 as.json.geojson <- function(x, pretty=TRUE, auto_unbox=TRUE, ...) {
   jsonlite::toJSON(unclass(x), pretty=pretty, auto_unbox=auto_unbox, ...)
 }
