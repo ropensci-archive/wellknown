b2e99982962c42912d621833a664b0cdc9327cae
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-20 12:21:34 -0700

    changed as.json name to as_json

--- R/as.json.R
+++ R/as.json.R
@@ -14,13 +14,13 @@
 #' @examples
 #' str <- "POLYGON ((100 0.1, 101.1 0.3, 101 0.5, 100 0.1),
 #'    (103.2 0.2, 104.8 0.2, 100.8 0.8, 103.2 0.2))"
-#' as.json(wkt2geojson(str))
-#' as.json(wkt2geojson(str), FALSE)
-as.json <- function(x, pretty=TRUE, auto_unbox=TRUE, ...) {
-  UseMethod("as.json")
+#' as_json(wkt2geojson(str))
+#' as_json(wkt2geojson(str), FALSE)
+as_json <- function(x, pretty=TRUE, auto_unbox=TRUE, ...) {
+  UseMethod("as_json")
 }
 
 #' @export
-as.json.geojson <- function(x, pretty=TRUE, auto_unbox=TRUE, ...) {
-  jsonlite::toJSON(unclass(x), pretty=pretty, auto_unbox=auto_unbox, ...)
+as_json.geojson <- function(x, pretty = TRUE, auto_unbox = TRUE, ...) {
+  jsonlite::toJSON(unclass(x), pretty = pretty, auto_unbox = auto_unbox, ...)
 }
