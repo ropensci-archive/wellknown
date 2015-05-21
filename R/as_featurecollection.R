9753a6992a356b1fc89318808be5d320649d3ecc
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-21 10:58:25 -0700

    commented out eg calling lawn code, fixed author fiedl

--- R/as_featurecollection.R
+++ R/as_featurecollection.R
@@ -7,9 +7,9 @@
 #' @examples
 #' str <- 'MULTIPOINT ((100.000 3.101), (101.000 2.100), (3.140 2.180),
 #' (31.140 6.180), (31.140 78.180))'
-#' wkt2geojson(str, fmt = 2) %>%
-#'   as_featurecollection() %>%
-#'   lawn::view()
+#' # wkt2geojson(str, fmt = 2) %>%
+#' #   as_featurecollection() %>%
+#' #   lawn::view()
 as_featurecollection <- function(x) {
   if (!is(x, "geojson")) stop("Must be of class geojson", call. = FALSE)
   x <- unclass(x)
