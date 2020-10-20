d4570bdba214fa4efcb6f8f6a6aa8b0ace3c5b00
Scott Chamberlain <myrmecocystus@gmail.com>
2020-10-20 09:42:26 -0700

    change input_size in bounding_wkt_points to signed int

--- src/bounding_wkt.cpp
+++ src/bounding_wkt.cpp
@@ -7,7 +7,7 @@ using namespace wkt_utils;
 //[[Rcpp::export]]
 CharacterVector bounding_wkt_points(NumericVector min_x, NumericVector max_x, NumericVector min_y, NumericVector max_y){
 
-  unsigned int input_size = min_x.size();
+  signed int input_size = min_x.size();
   if(max_x.size() != input_size || min_y.size() != input_size || max_y.size() != input_size){
     Rcpp::stop("All input vectors must be the same length");
   }
